842aaf57bc38c2bfba1bae21490a80660c82d3ad
karthik <karthik.moc@gmail.com>
2015-02-09 17:07:23 +0530

    Social distance code added up

--- src/main/java/com/nikgraph/social/NikGraph.java
+++ src/main/java/com/nikgraph/social/NikGraph.java
@@ -1,17 +1,14 @@
 package com.nikgraph.social;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Class for NikGraph data structure.
  * @param <T>
  */
-public class NikGraph<U, V, T extends GNode<U, V>> {
+public class NikGraph<U, T extends GNode<U, U>> {
 
-    Map<U, List<V>> keysMap = new HashMap<>();
+    Map<U, T> keysMap = new HashMap<>();
 
     public NikGraph() {
 
@@ -19,11 +16,60 @@ public class NikGraph<U, V, T extends GNode<U, V>> {
 
     public void addNode(T node) {
         U primaryId = node.getPrimaryId();
-        keysMap.put(primaryId, node.getValuesList());
+        keysMap.put(primaryId, node);
+    }
+
+    public int findSocialDistance(T node1, T node2) {
+        if(node1.equals(node2)) {
+            return 0;
+        } else {
+            return doBFS(node1, node2);
+        }
+    }
+
+    private int doBFS(T node1, T node2) {
+        Queue<U> fifoQueue = new ArrayDeque<>();
+        Map<U, U> visitedMap = new HashMap<>();
+
+        fifoQueue.add(node1.getPrimaryId());
+        visitedMap.put(node1.getPrimaryId(), null);
+
+        while (fifoQueue.size() > 0) {
+            U element = fifoQueue.element();
+            fifoQueue.remove();
+            if(element != null) {
+                T node = this.getNode(element);
+                if(node != null) {
+                    List<U> valuesList = node.getValuesList();
+                    for(U value : valuesList) {
+                        if(visitedMap.containsKey(value)) {
+                            continue;
+                        } else {
+                            if(value.equals(node2.getPrimaryId())) {
+                                return 1 + reachLength(element, visitedMap);
+                            } else {
+                                fifoQueue.add(value);
+                                visitedMap.put(value, element);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private int reachLength(U element, Map<U, U> visitedMap) {
+        U prevElement = visitedMap.get(element);
+        if(prevElement == null) {
+            return 0;
+        }
+        return 1 + reachLength(prevElement, visitedMap);
     }
 
     public void prettyPrintNode(T node) {
-        List<V> values = node.getValuesList();
+        List<U> values = node.getValuesList();
 
         // To save from Null pointer Exception.
         if(values == null) {
@@ -31,9 +77,16 @@ public class NikGraph<U, V, T extends GNode<U, V>> {
         }
 
         System.out.println("Node contents of " + node.getPrimaryId() + ": ");
-        for(V value : values) {
+        for(U value : values) {
             System.out.print(value.toString() + " -> ") ;
         }
         System.out.print("END");
+    }
+
+    public T getNode(U key) {
+        if(keysMap.containsKey(key)) {
+            return keysMap.get(key);
+        }
+        return null;
     }
 }
--- src/test/java/BasicGraphTests.java
+++ src/test/java/BasicGraphTests.java
@@ -1,6 +1,7 @@
 import com.nikgraph.social.GNode;
 import com.nikgraph.social.NikGraph;
 import com.nikgraph.social.UserNode;
+import junit.framework.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ public class BasicGraphTests {
 
     @Test
     public void basicGenerateGraphTestsSingleNodeWithoutValues() {
-        NikGraph<Integer, Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
+        NikGraph<Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
 
         List<Integer> contactsList = null;
         Integer userId = 0;
@@ -26,7 +27,7 @@ public class BasicGraphTests {
 
     @Test
     public void basicGenerateGraphTestsSingleNodeWithValues() {
-        NikGraph<Integer, Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
+        NikGraph<Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
 
         List<Integer> contactsList = null;
         Integer userId = 0;
@@ -36,6 +37,87 @@ public class BasicGraphTests {
         nikGraph.addNode(node);
 
         nikGraph.prettyPrintNode(node);
+    }
+
+    @Test
+    public void basicGenerateGraphTestsSeveralWithValues() {
+        NikGraph<Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
+
+        List<Integer> contactsList = null;
+        int userId = 0;
+
+        for(userId = 0; userId < 100; userId++) {
+            contactsList = populateContactsList(0, new Random().nextInt(50));
+            GNode node = new UserNode(contactsList, userId);
+            nikGraph.addNode(node);
+
+            System.out.println(node.getValuesList().size());
+        }
+    }
+
+    @Test
+    public void addContactTest() {
+        NikGraph<Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
+
+        Integer userId = 0;
+        addNewContacts(nikGraph, userId, 1);
+        Assert.assertEquals(nikGraph.getNode(0).getValuesList().size(), nikGraph.getNode(1).getValuesList().size());
+    }
+
+    @Test
+    public void socialDistanceTestsImmediateContact() {
+        NikGraph<Integer, GNode<Integer, Integer>> nikGraph = new NikGraph<>();
+
+        Integer userId = 0;
+        int contactId = 1;
+        addNewContacts(nikGraph, userId, contactId);
+
+        userId = 1;
+        contactId = 2;
+        addNewContacts(nikGraph, userId, contactId);
+
+        userId = 2;
+        contactId = 3;
+        addNewContacts(nikGraph, userId, contactId);
+
+        userId = 3;
+        contactId = 4;
+        addNewContacts(nikGraph, userId, contactId);
+
+        int socialDistance = nikGraph.findSocialDistance(nikGraph.getNode(0), nikGraph.getNode(4));
+        System.out.println(socialDistance);
+
+        userId = 4;
+        contactId = 0;
+        addNewContacts(nikGraph, userId, contactId);
+
+        socialDistance = nikGraph.findSocialDistance(nikGraph.getNode(0), nikGraph.getNode(4));
+        System.out.println(socialDistance);
+    }
+
+    private void addNewContacts(NikGraph<Integer, GNode<Integer, Integer>> nikGraph, Integer userId, int contactId) {
+        UserNode node = (UserNode) nikGraph.getNode(userId);
+
+        if(node == null) {
+            node = new UserNode(null, userId);
+        }
+        node.addContact(contactId);
+
+        GNode<Integer, Integer> contactsNode = nikGraph.getNode(contactId);
+        List<Integer> contactsListOfContact = null;
+        if(contactsNode != null) {
+            contactsNode.getValuesList();
+        }
+
+        if(contactsListOfContact == null || (!contactsListOfContact.contains(userId))) {
+            if(contactsListOfContact == null) {
+                contactsListOfContact = new ArrayList<>();
+            }
+            contactsListOfContact.add(userId);
+            nikGraph.addNode(new UserNode(contactsListOfContact, contactId));
+        }
+
+        nikGraph.addNode(node);
     }
 
     private List<Integer> populateContactsList(int exclude, int size) {
